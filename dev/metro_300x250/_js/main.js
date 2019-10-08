3ae9d6a9317c9a0cfe19edc91a2eb941282fe743
Gar <gar@lonelydatu.com>
2019-10-07 23:05:30 -0400

    one

---
+++ dev/metro_300x250/_js/main.js
@@ -0,0 +1,29 @@
+
+
+
+function start(){
+	const tl = new TimelineMax()
+	tl.set('.frame1', {opacity:1})
+
+	tl.from('.t1', .3, { opacity: 0 }, '+=.3')
+	tl.to('.t1', .3, { opacity: 0 }, '+=1')
+
+
+
+	tl.add('t3')
+	tl.from('.t3a', .3, { opacity: 0 }, 't3')
+	tl.from(['.logo_royal', '.logo_spot'], .3, { opacity: 0 }, 't3' )
+
+	tl.add('t3b', "+=3")
+	tl.to('.t3a', .3, { y:0 }, "t3b")
+	tl.from('.t3b', .3, { opacity: 0 }, "t3b")
+	tl.from('.logo_spot', .3, { opacity: 0 }, "t3b")
+	tl.from('.cta', .3, { opacity: 0, y:"+=15" }, "+=.1")	
+}
+
+start()
+
+
+
+module.exports = {};
+
