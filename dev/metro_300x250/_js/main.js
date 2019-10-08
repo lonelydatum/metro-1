8d76e01a97863214a18af77e398286ce1f3d881e
Gar <gar@lonelydatu.com>
2019-10-08 09:44:54 -0400

    one

--- dev/metro_300x250/_js/main.js
+++ dev/metro_300x250/_js/main.js
@@ -1,26 +1,27 @@
-
+import '../../_common/js/common.js'
 
 
 function start(){
 	const tl = new TimelineMax()
 	tl.set('.frame1', {opacity:1})
 
-	tl.from('.t1', .3, { opacity: 0 }, '+=.3')
+	tl.from('.t1', .5, { opacity: 0 }, '+=.3')
 	tl.to('.t1', .3, { opacity: 0 }, '+=1')
 
 
 
-	tl.add('t3')
-	tl.from('.t3a', .3, { opacity: 0 }, 't3')
-	tl.from(['.logo_royal', '.logo_spot'], .3, { opacity: 0 }, 't3' )
+	tl.add('t3', "+=.3")
+	tl.from('.t3a', .5, { opacity: 0 }, 't3')
+	tl.from(['.logo_royal', '.logo_spot'], .3, { opacity: 0 } )
 
-	tl.from('.logo_spot', .3, { opacity: 0 }, "+=3")
-
-	tl.add('t3b')
-	tl.to('.t3a', .3, { y:0 }, "t3b")
-	tl.from('.t3b', .3, { opacity: 0 }, "t3b")
 	
-	tl.from('.cta', .3, { opacity: 0, y:"+=20" }, "t3b")	
+
+	tl.add('end', "+=3.2")
+	tl.to('.logo_spot', .5, { y:"+=10", opacity: 0 }, 'end-=.2')
+	tl.to('.t3a', .3, { y:0 }, "end")		
+	tl.from('.cta', .5, { opacity: 0, y:"+=20" }, "end")	
+
+	tl.from('.t3b', 1, {clip: `rect(0px 0px 500px 0px)`, ease:Power3.easeOut }, "+=.3")
 }
 
 start()
