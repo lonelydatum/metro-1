94bbc5fce83c7e2455ad81490f4236727e6a1b14
Gar <gar@lonelydatu.com>
2019-10-07 23:16:30 -0400

    one

--- dev/metro_300x250/_bundled/main.js
+++ dev/metro_300x250/_bundled/main.js
@@ -12,11 +12,13 @@ function start() {
 	tl.from('.t3a', .3, { opacity: 0 }, 't3');
 	tl.from(['.logo_royal', '.logo_spot'], .3, { opacity: 0 }, 't3');
 
-	tl.add('t3b', "+=3");
+	tl.from('.logo_spot', .3, { opacity: 0 }, "+=3");
+
+	tl.add('t3b');
 	tl.to('.t3a', .3, { y: 0 }, "t3b");
 	tl.from('.t3b', .3, { opacity: 0 }, "t3b");
-	tl.from('.logo_spot', .3, { opacity: 0 }, "t3b");
-	tl.from('.cta', .3, { opacity: 0, y: "+=15" }, "+=.1");
+
+	tl.from('.cta', .3, { opacity: 0, y: "+=20" }, "t3b");
 }
 
 start();
