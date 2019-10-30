8fcae54b8b1604015b3b6307c828b2261bd4a6f6
Gar <gar@lonelydatu.com>
2019-10-30 17:05:03 -0400

    2

--- _1/metro_300x250/_bundled/main.js
+++ _1/metro_300x250/_bundled/main.js
@@ -20,22 +20,19 @@ function start() {
 	var tl = new TimelineMax();
 	tl.set('.frame1', { opacity: 1 });
 
-	tl.from('.logo_metro', .3, { opacity: 0 });
-	// tl.from('.line_hor', .5, { opacity: 0 })
-	tl.from('.logo_ls', .3, { opacity: 0 }, "+=.2");
-	tl.to(".f1", .35, { opacity: 0 }, '+=1.5');
-
 	tl.from('.t1', .5, { opacity: 0 }, '+=.3');
+	tl.to('.t1', .3, { opacity: 0 }, '+=1');
 
-	tl.to('.t1', .3, { opacity: 0 }, '+=2');
+	tl.add('t3', "+=.3");
+	tl.from('.t3a', .5, { opacity: 0 }, 't3');
+	tl.from(['.logo_royal', '.logo_spot'], .3, { opacity: 0 });
 
-	tl.from('.t2', .5, { opacity: 0 });
-	tl.from('.logo_royal', .5, { opacity: 0 }, '+=.3');
-	tl.to('.f2', .3, { opacity: 0 }, '+=4');
-
-	tl.from('.logos', .3, { opacity: 0 });
-
+	tl.add('end', "+=1.7");
+	tl.to('.logo_spot', .5, { y: "+=10", opacity: 0 }, 'end-=.2');
+	tl.to('.t3a', .3, { y: 0 }, "end");
 	tl.from('.cta', .5, { opacity: 0, y: "+=20" }, "end");
+
+	tl.from('.t3b', 1, { clip: 'rect(0px 0px 500px 0px)', ease: Power3.easeOut }, "+=.3");
 }
 
 start();
---
+++ _1/metro_300x250/_js/main.js
@@ -0,0 +1,32 @@
+import '../../_common/js/common.js'
+
+
+function start(){
+	const tl = new TimelineMax()
+	tl.set('.frame1', {opacity:1})
+
+	tl.from('.t1', .5, { opacity: 0 }, '+=.3')
+	tl.to('.t1', .3, { opacity: 0 }, '+=1')
+
+
+
+	tl.add('t3', "+=.3")
+	tl.from('.t3a', .5, { opacity: 0 }, 't3')
+	tl.from(['.logo_royal', '.logo_spot'], .3, { opacity: 0 } )
+
+	
+
+	tl.add('end', "+=1.7")
+	tl.to('.logo_spot', .5, { y:"+=10", opacity: 0 }, 'end-=.2')
+	tl.to('.t3a', .3, { y:0 }, "end")		
+	tl.from('.cta', .5, { opacity: 0, y:"+=20" }, "end")	
+
+	tl.from('.t3b', 1, {clip: `rect(0px 0px 500px 0px)`, ease:Power3.easeOut }, "+=.3")
+}
+
+start()
+
+
+
+module.exports = {};
+
